02b7d3bbf2cb3c6c3cd350f498cac3c6c863e8e3
Brandon Lew <25602300+blsnwbrdr@users.noreply.github.com>
2025-12-04 11:40:58 -0800

    chore: update Angular to v19 and apply migrations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,10 @@ import { ICountry } from './interfaces/country.model';
 import { CountriesService } from './services/countries.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    imports: [RouterOutlet, CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   isOverlay = false;
